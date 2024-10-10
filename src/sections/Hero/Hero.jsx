94d09c4a08393d6a1f0f57214085ae99d96ec39a
Lucio Torelli <luciotorelli@hotmail.com>
2024-10-10 14:58:34 +0100

    Add ThemeContext and ThemeProvider components

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,34 +1,59 @@
-import styles from './HeroStyles.module.css'
-import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/Design uten navn.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
   return (
-    <section id="hero" className='{styles.container}'>
-    <div className={styles.colorModeContainer}>
-        <img className={styles.hero} src={heroImg} alt='Profile picture of Lúcio Torelli'/>
-        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
-    </div>
-    <div className='{styles.info}'>
-        <h1>Lúcio Torelli</h1>
-        <h2>Full-stack Developer</h2>
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Harris
+          <br />
+          Johnsen
+        </h1>
+        <h2>Full-stack developer</h2>
         <span>
-            <a href="https://github.com/luciotorelli" target='_blank'>
-                <img src={githubIcon} alt="GitHub badge" />
-            </a>
-            <a href="https://www.linkedin.com/in/luciotorelli" target='_blank'>
-                <img src={linkedinIcon} alt="LinkedIn badge" />
-            </a>
+          <a href="https://github.com/luciotorelli" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/in/luciotorelli" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
         </span>
-        <p>Driven by a lifelong passion for tech and always curious to learn more. Focused on growing my skills and tackling challenges while adding value to my team and the company.</p>
+        <p className={styles.description}>
+        <h2>Full-stack Developer</h2>
+        </p>
         <a href="" download>
-            <button className='hover'>Download Resume</button>
+          <button className="hover">Resume</button>
         </a>
-    </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
